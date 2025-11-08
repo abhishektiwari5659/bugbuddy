1143df41d55668cb745171b45c30ca62f8363822
HeheAbhi <abhishekti5659@gmail.com>
2025-11-08 16:27:14 +0530

    added cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 import express from "express";
 import connection from "./config/database.js";
 import cookieParser from "cookie-parser";
+import cors from "cors"
 import { authRouter } from "./routes/auth.js";
 import { profileRouter } from "./routes/profile.js";
 import { requestRouter } from "./routes/requests.js";
 import {userRouter} from "./routes/user.js"
+
 const app = express();
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}))
 app.use(express.json());
 app.use(cookieParser());
 app.use("/", authRouter);
